7cc2d56a320f9d0b16b0b8297fcd23e4009c3a1e
Yevgeny V.Yorkhov <yy@ixi.ru>
2016-03-13 11:16:36 +0300

    Add books and week2_helper

--- code/raw/week2_helper.R
+++ code/raw/week2_helper.R
@@ -1,16 +1,28 @@
 library(tm)
 
-file.blog="data/final/en_US/en_US.blogs.txt"
-file.news="data/final/en_US/en_US.news.txt"
-file.twitter="data/final/en_US/en_US.twitter.txt"
+dirEN <- "data/final/en_US/small"
+blog.en="data/final/en_US/en_US.blogs.txt"
+news.en="data/final/en_US/en_US.news.txt"
+twitter.en="data/final/en_US/en_US.twitter.txt"
+blog.ru="data/final/ru_RU/ru_RU.blogs.txt"
+news.ru="data/final/ru_RU/ru_RU.news.txt"
+twitter.ru="data/final/ru_RU/ru_RU.twitter.txt"
 
-conn <- file(file.blog,open="r")
+# Load file line by line
+conn <- file(blog.en,open="r")
 raw <- readLines(conn)
 close(conn)
 
-txt <- VectorSource(raw[1:100])
-txt.corpus <- Corpus(txt)
+tmp <- raw[1:1000]
+tmp <- tmp[grep("[a-zA-Z0-9 ]+", tmp)]
+
+txt <- VectorSource(tmp)
+txt.corpus <- Corpus(txt, readerControl = txt$DefaultReader, Encoding="latin1")
 inspect(txt.corpus)
+
+# Load file using tm
+#blog <- system.file("data/final/en_US/en_US.blogs.txt", package = "tm")
+#txt.corpus <- Corpus(DirSource(dirEN), readerControl = list(reader=readPlain, language="la", load=TRUE))
 
 txt.corpus <- tm_map(txt.corpus, content_transformer(tolower))
 txt.corpus <- tm_map(txt.corpus, removePunctuation)
@@ -18,10 +30,10 @@ txt.corpus <- tm_map(txt.corpus, removeNumbers)
 txt.corpus <- tm_map(txt.corpus, removeWords, stopwords("english"))
 
 # Stem documents
-library(SnowballC)
-txt.corpus <- tm_map(txt.corpus, stemDocument)
-detach(package:SnowballC)
-inspect(txt.corpus)
+# library(SnowballC)
+# txt.corpus <- tm_map(txt.corpus, stemDocument)
+# detach(package:SnowballC)
+# inspect(txt.corpus)
 
 txt.corpus <- tm_map(txt.corpus, stripWhitespace)
 inspect(txt.corpus)
@@ -30,8 +42,18 @@ inspect(txt.corpus)
 tdm <- TermDocumentMatrix(txt.corpus)
 inspect(tdm)
 
-# Find all words/terms which were used 8 or more times (in all docs)
-findFreqTerms(x=tdm, lowfreq = 25, highfreq = Inf)
+# Clear tdm
+#rowTotals <- apply(tdm , 1, sum) #Find the sum of words in each Document
+#tdm   <- tdm[rowTotals > 0, ]           #remove all docs without words
+
+# Find associated words
+findAssocs(x=tdm, term="afford", corlimit=0.6)
+
+# remove sparse terms (which occur very infrequently)
+tdm.common.60 <- removeSparseTerms(tdm, 0.6)
+tdm.common.20 <- removeSparseTerms(x=tdm, sparse = 0.2)
+
+findFreqTerms(x=tdm, lowfreq = 100, highfreq = Inf)
 
 
 
