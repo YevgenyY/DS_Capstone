40a81287a792c0c1364d4e797cad0813399ddf31
Yevgeny V. Yorkhov <yy@ixi.ru>
2016-03-25 20:04:28 +0300

    Commit for the code transfer.

--- code/raw/mle_helper.R
+++ code/raw/mle_helper.R
@@ -3,6 +3,10 @@ library(quanteda)
 setwd("~/Coursera/DS_Capstone/")
 load(file="data/freq_tokens_dfm.Rda")
 
+q1 <- "The guy in front of me just bought a pound of bacon, a bouquet, and a case of"
+alphabet.en <- c(stopwords("english"), 
+                 "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","q","p","r","s","t","u","v","w","x","y","z")
+
 # return word #n-1
 wnm1 <- function(x) { return(strsplit(x, " ")[[1]][1]) }
 
@@ -17,5 +21,14 @@ ngram_mask <- function(x, ngram) {
   return(subset(x, grepl(paste(pattern), names(x))))
 }
 
-head( ngram_mask(freq_two, "years") )
+head( ngram_mask(freq_two, "case") )
+
+raw <- q1
+raw <- gsub("[^[:alnum:][:space:]']", ' ', raw)
+raw <- gsub('[[:digit:]]+', ' ', raw)
+raw <- gsub('[[:punct:]]+', '', raw)
+raw <- tolower(raw)
+
+tokens <- tokenize(raw, simplify=FALSE)
+tokens <- removeFeatures(tokens, c(stopwords("english"), "will", "ass", alphabet.en, profanityWords.en))
 
